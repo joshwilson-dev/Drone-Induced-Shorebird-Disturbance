836534915ae9c240023da9af1f4cfedb63cf3c10
Joshua Wilson <joshua.wilson1@uq.net.au>
2022-02-28 16:36:38 +1000

    trying to get this working

--- scripts/GAM.R
+++ scripts/GAM.R
@@ -150,74 +150,6 @@ data_aug <- data %>%
     # make column names correct format for gam
     rename_all(make.names)
 
-#### Ethics Analysis ####
-
-# number of approaches
-approaches <- data_aug %>%
-    # flights not in 2021
-    filter(datetime_aest > as.Date("2021-01-01")) %>%
-    filter(datetime_aest < as.Date("2022-01-01")) %>%
-    # remove flights where birds took off in control
-    filter(approach.type != "control") %>%
-    # group by flight
-    group_by(flightcode) %>%
-    # take first entry
-    slice(1)
-
-# number of disturbances
-disturbance_approaches <- data_aug %>%
-    # flights not in 2021
-    filter(datetime_aest > as.Date("2021-01-01")) %>%
-    filter(datetime_aest < as.Date("2022-01-01")) %>%
-    # only keep flights where drone was approaching
-    filter(
-        approach.type == "ascending" |
-        approach.type == "advancing") %>%
-    # filter for birds fligt
-    filter(behaviour == 2) %>%
-    # remove disturbances from alternate sources
-    mutate(notes = case_when(
-        notes == "alternate disturbance" ~ 1,
-        TRUE ~ 2)) %>%
-    filter(notes == 2) %>%
-    # group by flight
-    group_by(flightcode) %>%
-    # take first entry
-    slice(1)
-
-# total birds disturbed
-birds_disturbed <- data_aug  %>%
-    # filter for birds flight
-    filter(behaviour == 2) %>%
-    # group each species for each flight
-    group_by(flightcode, species) %>%
-    # take first entry
-    slice(1) %>%
-    # only keep flights where drone was approaching
-    filter(
-        approach.type == "ascending" |
-        approach.type == "advancing") %>%
-    # remove disturbances from alternate sources
-    mutate(notes = case_when(
-        notes == "alternate disturbance" ~ 1,
-        TRUE ~ 2)) %>%
-    filter(notes == 2) %>%
-    # flights not in 2021
-    filter(datetime_aest > as.Date("2021-01-01")) %>%
-    filter(datetime_aest < as.Date("2022-01-01")) %>%
-    # summarise
-    group_by(species) %>%
-    summarise(sum = sum(count))
-(sum(birds_disturbed$sum) - 4000)
-
-# species
-species <- data_aug %>%
-    # filter out species without enough data
-    group_by(species) %>%
-    filter(n() > 5) %>%
-    # take first entry to get unique species
-    slice(1)
-
 #### fia analysis ####
 data_fia <- data_aug %>%
     # keep only flights where the drone was advancing
@@ -619,6 +551,75 @@ ggplot() +
         expand = c(0, 0),
         breaks = round(seq(0, 800, by = 100), 1)) +
     scale_y_continuous(expand = c(0, 0))
+
+
+#### Ethics Analysis ####
+
+# number of approaches
+approaches <- data_aug %>%
+    # flights not in 2021
+    filter(datetime_aest > as.Date("2021-01-01")) %>%
+    filter(datetime_aest < as.Date("2022-01-01")) %>%
+    # remove flights where birds took off in control
+    filter(approach.type != "control") %>%
+    # group by flight
+    group_by(flightcode) %>%
+    # take first entry
+    slice(1)
+
+# number of disturbances
+disturbance_approaches <- data_aug %>%
+    # flights not in 2021
+    filter(datetime_aest > as.Date("2021-01-01")) %>%
+    filter(datetime_aest < as.Date("2022-01-01")) %>%
+    # only keep flights where drone was approaching
+    filter(
+        approach.type == "ascending" |
+        approach.type == "advancing") %>%
+    # filter for birds fligt
+    filter(behaviour == 2) %>%
+    # remove disturbances from alternate sources
+    mutate(notes = case_when(
+        notes == "alternate disturbance" ~ 1,
+        TRUE ~ 2)) %>%
+    filter(notes == 2) %>%
+    # group by flight
+    group_by(flightcode) %>%
+    # take first entry
+    slice(1)
+
+# total birds disturbed
+birds_disturbed <- data_aug  %>%
+    # filter for birds flight
+    filter(behaviour == 2) %>%
+    # group each species for each flight
+    group_by(flightcode, species) %>%
+    # take first entry
+    slice(1) %>%
+    # only keep flights where drone was approaching
+    filter(
+        approach.type == "ascending" |
+        approach.type == "advancing") %>%
+    # remove disturbances from alternate sources
+    mutate(notes = case_when(
+        notes == "alternate disturbance" ~ 1,
+        TRUE ~ 2)) %>%
+    filter(notes == 2) %>%
+    # flights not in 2021
+    filter(datetime_aest > as.Date("2021-01-01")) %>%
+    filter(datetime_aest < as.Date("2022-01-01")) %>%
+    # summarise
+    group_by(species) %>%
+    summarise(sum = sum(count))
+(sum(birds_disturbed$sum) - 4000)
+
+# species
+species <- data_aug %>%
+    # filter out species without enough data
+    group_by(species) %>%
+    filter(n() > 5) %>%
+    # take first entry to get unique species
+    slice(1)
 
 #### PLAYING AROUND ####
 summary(data)
